2cdf15102f394fd886df6bc93af01dff433f488c
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-06-24 21:44:33 -0400

    feat: add precio atributte

--- helia_hotel/lib/model/data_habitacion.dart
+++ helia_hotel/lib/model/data_habitacion.dart
@@ -2,11 +2,13 @@ class Habitacion {
   final String tipoHabitacion;
   final int cantidad;
   final String codHabitacion;
+  final double precio;
 
   Habitacion({
     required this.tipoHabitacion,
     required this.cantidad,
     required this.codHabitacion,
+    required this.precio,
   });
 
   factory Habitacion.fromJson(Map<dynamic, dynamic> json) {
@@ -14,11 +16,14 @@ class Habitacion {
       tipoHabitacion: json['tipoHabitacion'] ?? '',
       cantidad: json['cantidad'] ?? 0,
       codHabitacion: json['codHabitacion'] ?? '',
+      precio:
+          (json['precio'] ?? 0.0).toDouble(), // Asegura la conversión a double
     );
   }
 
   Habitacion.empty()
       : tipoHabitacion = '',
         cantidad = 0,
-        codHabitacion = '';
+        codHabitacion = '',
+        precio = 0.0;
 }
